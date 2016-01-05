3268a9efa79b1edd30d68b2d314e23f34b1d31e8
hanks-zyh <zhangyuhan2014@gmail.com>
2016-01-06 01:25:49 +0800

    Site updated: 2016-01-06 01:25:48

--- js/script.js
+++ js/script.js
@@ -132,3 +132,20 @@
   });
 
 })(jQuery);
+//scroll
+
+$(function () {
+  var ie6 = document.all;
+  var dv = $('#sidebar'), st;
+  dv.attr('top',dv.offset().top);
+  dv.attr('otop', dv.offset().top + dv.outerHeight(true) - $(window).height() ); //存储原来的距离顶部的距离
+  $(window).scroll(function () {
+    st = Math.max(document.body.scrollTop || document.documentElement.scrollTop);
+    if (st > parseInt(dv.attr('otop'))) {
+      if (ie6) {//IE6不支持fixed属性，所以只能靠设置position为absolute和top实现此效果
+       dv.css({ position: 'absolute', top: st });
+      }
+      else if (dv.css('position') != 'fixed') dv.css({ 'position': 'fixed', top: dv.attr('top')-dv.attr('otop') });
+    } else if (dv.css('position') != 'static') dv.css({ 'position': 'static' });
+  });
+});
